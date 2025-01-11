d5a6ee0365a69a21b7100f1ca58c0b4230dff90c
venkata phani raju meda <vmeda@twilio.com>
2025-01-11 17:05:48 +0530

    updated  app.json  file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-'use strict';
+'use strict'
 
-const fastify = require('fastify')();
-const path = require('node:path');
-const AutoLoad = require('@fastify/autoload');
+const path = require('node:path')
+const AutoLoad = require('@fastify/autoload')
 const Swagger = require('@fastify/swagger');
 const SwaggerUI = require('@fastify/swagger-ui');
 
 // Pass --options via CLI arguments in command to enable these options.
-const options = {};
+const options = {}
 
-// Register plugins and routes
-async function buildApp() {
+module.exports = async function (fastify, opts) {
+
     // Place here your custom code!
     fastify.register(Swagger, {
         mode: 'static',
@@ -25,39 +24,21 @@ async function buildApp() {
     });
 
     // Do not touch the following lines
+
     // This loads all plugins defined in plugins
     // those should be support plugins that are reused
     // through your application
     fastify.register(AutoLoad, {
         dir: path.join(__dirname, 'plugins'),
-        options: Object.assign({}, options)
-    });
+        options: Object.assign({}, opts)
+    })
 
     // This loads all plugins defined in routes
     // define your routes in one of these
     fastify.register(AutoLoad, {
         dir: path.join(__dirname, 'routes'),
-        options: Object.assign({}, options)
-    });
-
-    return fastify;
+        options: Object.assign({}, opts)
+    })
 }
 
-(async () => {
-    try {
-        const PORT = process.env.PORT || 3000; // Use PORT from Heroku or default to 3000
-        const app = await buildApp();
-
-        // Start the server
-        app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            console.log(`Server is running at ${address}`);
-        });
-    } catch (err) {
-        console.error('Error starting server:', err);
-        process.exit(1);
-    }
-})();
+module.exports.options = options
