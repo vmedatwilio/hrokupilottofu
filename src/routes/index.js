36a4db6389d4429b0cd1ff34436cec9cc444cc42
venkata phani raju meda <vmeda@twilio.com>
2025-02-06 14:38:23 +0530

    updated app js filewith activites fetch without local

--- src/routes/index.js
+++ src/routes/index.js
@@ -276,7 +276,7 @@ module.exports = async function (fastify, opts) {
                 }
     
                 logger.info(`Total activities fetched: ${activities.length}`);
-                return reply.send({ 'activities':activities.length });
+                return activities;
     
             } catch (e) {
                 logger.error(`Error querying activities: ${e.message}`);
